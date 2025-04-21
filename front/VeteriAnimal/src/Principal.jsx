134229c8b518f2ae44b4db95e50e3398bf9a375f
JarbeyOcampo1 <164644585+JarbeyOcampo1@users.noreply.github.com>
2025-04-21 17:51:19 -0500

    ADD
    pagina principal

--- front/VeteriAnimal/src/Principal.jsx
+++ front/VeteriAnimal/src/Principal.jsx
@@ -1,11 +1,73 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Logo1 from "./components/image/Logo1.jpg";
+import imagen4 from './components/image/imagen4.jpg'
+import imagen2 from './components/image/imagen2.jpg'
+import imagen3 from './components/image/imagen3.jpg'
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import './Principal.css'
 
 function Principal () {
+
+    // Inicializamos la función de navegación
+    const navigate = useNavigate() 
+    
+    // Verifica si el token de autenticación existe en el almacenamiento local
+    useEffect ( () => {
+        const auntenticar = localStorage.getItem("Exito");
+        if (!auntenticar) {
+            navigate("/");
+        }
+    },[navigate]);
+
+    // Función para cerrar sesión
+    const handleLogout = () => {
+        localStorage.removeItem("Exito");
+        navigate("/");
+    };
+
     return (
         <div>
-            <h1>HOLA</h1>
+          <div className="Container_Principal">
+            <div className="logo-container">
+            <img src={Logo1} alt="Logo principal" className="principal-logo-image" />
+            </div>
+             {/* Barra de navegación con el botón de salir */}
+            <nav className="navbar">
+              <button onClick={handleLogout} className="logout-button">Salir</button>
+            </nav>
+          </div>
+          {/* Contenedor del título con animación */}
+          <div className="title-container">
+          <h1 className="title"> <span>V</span><span>E</span><span>T</span><span>E</span><span>R</span><span>I</span>
+          <span>A</span><span>N</span><span>I</span><span>M</span><span>A</span><span>L</span></h1>
+          </div>
+           {/* Carrusel de imágenes (simulado con scroll horizontal) */}
+          <div className="carrousel">
+            <div><img src={imagen4}  alt="imagen1"/></div>
+            <div><img src={imagen2} alt="imagen2"/></div>
+            <div><img src={imagen3} alt="imagen3"/></div>
+            <div><img src={imagen4}  alt="imagen1"/></div>
+            <div><img src={imagen2} alt="imagen2"/></div>
+            <div><img src={imagen3} alt="imagen3"/></div>
+            <div><img src={imagen4}  alt="imagen1"/></div>
+            <div><img src={imagen2} alt="imagen2"/></div>
+            <div><img src={imagen3} alt="imagen3"/></div>
+            <div><img src={imagen4}  alt="imagen1"/></div>
+            <div><img src={imagen2} alt="imagen2"/></div>
+            <div><img src={imagen3} alt="imagen3"/></div>
+          </div>
+           {/* Pie de página con enlaces a redes sociales */}
+          <footer className="footer">
+            <h2>Síguenos en nuestras redes sociales:</h2>
+            <div className="social-icons">
+              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+            </div>
+          </footer>
         </div>
-    );
+      );
 }
 
 export default Principal;
--- front/VeteriAnimal/src/components/C_Inicio_Sesion/LoginsForm.jsx
+++ front/VeteriAnimal/src/components/C_Inicio_Sesion/LoginsForm.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import './LoginsForm.css';
 
 function LoginsForm ({onSubmit, initialLogin}) {
 
